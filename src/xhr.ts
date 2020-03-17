170a808012034d87f59b399c4fe66a848872b705
moslixiansen <moslixiansen@163.com>
2020-03-17 22:42:27 +0800

    feat: add error handle
    add error handle class

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,9 +1,14 @@
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from './types'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest()
-    const { url, method = 'get', data = null, headers, responseType } = config
+    const { url, method = 'get', data = null, headers, responseType, timeout } = config
+
+    if (timeout) {
+      request.timeout = timeout
+    }
 
     if (responseType) {
       request.responseType = responseType
@@ -21,10 +26,23 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       }
     })
 
+    request.onerror = function handleNetworkError() {
+      reject(createError('Network Error!', config, '', request))
+    }
+
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout} ms exceed`, config, 'ECONNABORTED', request))
+    }
+
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
         return
       }
+
+      if (request.status === 0) {
+        return
+      }
+
       // 获取响应头
       const responseHeaders = request.getAllResponseHeaders()
       const responseData =
@@ -37,9 +55,20 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+
+      handleResponse(response)
     }
 
     request.send(data)
+
+    function handleResponse(res: AxiosResponse) {
+      if (res.status >= 200 && res.status <= 300) {
+        resolve(res)
+      } else {
+        reject(
+          createError(`Request failed with status code = ${res.status}`, config, '', request, res)
+        )
+      }
+    }
   })
 }
